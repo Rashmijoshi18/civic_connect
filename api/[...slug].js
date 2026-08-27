ed51add80beae63933923c0567cd58c6fc68d16b
Rashmi Joshi <rashmijoshi3699@gmail.com>
2026-08-27 15:35:30 +0530

    refactor: migrate Vercel configuration to serverless functions with a catch-all API route handler

---
+++ api/[...slug].js
@@ -0,0 +1,3 @@
+// Catch-all handler: routes every /api/* request to the Express app.
+// Vercel's Node File Tracing (NFT) bundles all dependencies automatically.
+module.exports = require('../server/server');
